1386f44e18717668729fd941ad8f63d83e4a7abc
Alex Semenov <alncaatl@gmail.com>
2022-05-02 21:25:53 +0300

    Add: even_numbers_game() draft

--- brain_games/even_numbers.py
+++ brain_games/even_numbers.py
@@ -1,3 +1,29 @@
-def even_numbers():
-    pass
+import prompt
+
+
+def check_answer(number, answer):
+    return answer == 'no'
+
+
+game_description = 'Answer "yes" if the number is even, otherwise answer "no".'
+
+
+def even_numbers_game(user):
+    print(game_description)
     
+    counter = 0
+
+    while counter < 3:
+        number = 25
+
+        print(f'Question: {number}')
+        answer = prompt.string('Your answer: ')
+
+        if check_answer(number, answer):
+            print('Correct!')
+            counter += 1
+        else:
+            print(f"'{answer}' is wrong answer ;(. Correct answer was another answer.\nLet's try again, {user}!")
+            return
+        
+    print(f'Congratulations, {user}!')
--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -4,15 +4,15 @@
 
 
 from brain_games.cli import welcome_user
-from brain_games.even_numbers import even_numbers
+from brain_games.even_numbers import even_numbers_game
 from brain_games.scripts.brain_games import greet
 
 
 def main():
     """Run a game."""
     greet()
-    welcome_user()
-    even_numbers()
+    name = welcome_user()
+    even_numbers_game(name)
 
 
 if __name__ == '__main__':

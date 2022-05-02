86e3de8a1a61d9b579d24a0ee6ffe5dcb7f40729
Alex Semenov <alncaatl@gmail.com>
2022-05-02 04:09:25 +0300

    Fix: linting errors

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -1,6 +1,9 @@
+"""Logic for CLI interactions with user."""
+
 import prompt
 
 
 def welcome_user():
+    """User acquaintance."""
     name = prompt.string('May I have your name? ')
     print(f'Hello, {name}!')
--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,13 +1,18 @@
 #!/usr/bin/env python
 
+"""The entry point for program."""
+
+
 from brain_games.cli import welcome_user
 
 
 def greet():
+    """Greeting on startup event."""
     print('Welcome to the Brain Games!')
 
 
 def main():
+    """Run a program."""
     greet()
     welcome_user()
 

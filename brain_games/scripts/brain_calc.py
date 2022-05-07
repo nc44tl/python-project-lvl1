a9de8dc68294e7afe53d0499bd1d6abd462de20f
Alex Semenov <alncaatl@gmail.com>
2022-05-07 20:22:23 +0300

    Add: brain-calc script

---
+++ brain_games/scripts/brain_calc.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+"""The entry point for brain-even game (is random number even)."""
+
+
+from brain_games.cli import welcome_user
+from brain_games.even_numbers_game import even_numbers_game
+from brain_games.scripts.brain_games import greet
+
+
+def main():
+    """Run a game."""
+    greet()
+    name = welcome_user()
+    even_numbers_game(name)
+
+
+if __name__ == '__main__':
+    main()

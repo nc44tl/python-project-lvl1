7b6e09ebff09f26fc4a7fb121835e5614af021ab
Alex Semenov <alncaatl@gmail.com>
2022-05-09 00:39:55 +0300

    Add: /games/gcd.py logic

--- brain_games/games/gcd.py
+++ brain_games/games/gcd.py
@@ -1,36 +1,39 @@
 """The calculation game. User solves generated cases."""
 
+import math
+
 import prompt
 from brain_games.cli import answer_prompt, question_string
 from brain_games.game import check_user_answer, get_number
 
 min_number = 1
-max_number = 10
+max_number = 50
 
 
 def _get_case():
-    a = get_number(min_number, max_number)  # noqa: WPS111 too short name
-    b = get_number(min_number, max_number)  # noqa: WPS111 too short name
+    num_a = get_number(min_number, max_number)
+    num_b = get_number(min_number, max_number)
+    case = '{num_a} {num_b}'.format(num_a=num_a, num_b=num_b)
 
-    return '{a} {b}'.format(a=a, b=b)
-
-
-def _get_correct_answer(case):
-    return case
+    return (case, num_a, num_b)
 
 
-def gcd_game():
+def _get_correct_answer(num_a, num_b):
+    return str(math.gcd(num_a, num_b))
+
+
+def gcd_game():  # noqa: WPS210 too many local variables
     """One round of brain-gcd game.
 
     Returns:
-        res = result of the round (True or False),
+        res = result of the round (boolean),
         user_unswer,
         correct_answer
     """
-    case = _get_case()
+    (case, num_a, num_b) = _get_case()
 
     print(question_string + case)
-    correct_answer = _get_correct_answer(case)
+    correct_answer = _get_correct_answer(num_a, num_b)
     user_answer = prompt.string(answer_prompt)
 
     res = check_user_answer(user_answer, correct_answer)

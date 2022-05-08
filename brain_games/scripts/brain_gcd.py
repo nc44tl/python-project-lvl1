4b4e9b9f3f760e5a72ef5d2c37d8d144e1a0a9de
Alex Semenov <alncaatl@gmail.com>
2022-05-08 23:32:57 +0300

    Add: /scripts/brain_gcd.py

---
+++ brain_games/scripts/brain_gcd.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+"""The entry point for brain-gcd game (find the greatest common divisor of given numbers)."""
+
+
+from brain_games.cli import greet, welcome_user
+from brain_games.game import game
+
+
+def main():
+    """Run a game."""
+    greet()
+    username = welcome_user()
+    game(username, 'brain-gcd')
+
+
+if __name__ == '__main__':
+    main()

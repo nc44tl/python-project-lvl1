930aa5a362240cc8d5ee343b3f66b396a71ab9a6
Alex Semenov <alncaatl@gmail.com>
2022-05-09 16:03:00 +0300

    Fix: constants and local variables naming issues

--- brain_games/games/gcd.py
+++ brain_games/games/gcd.py
@@ -3,16 +3,16 @@
 import math
 
 import prompt
-from brain_games.cli import answer_prompt, question_string
+from brain_games.cli import ANSWER_PROMPT, QUESTION_STRING
 from brain_games.game import check_user_answer, get_number
 
-min_number = 1
-max_number = 50
+_MIN_NUMBER = 1
+_MAX_NUMBER = 50
 
 
 def _get_case():
-    num_a = get_number(min_number, max_number)
-    num_b = get_number(min_number, max_number)
+    num_a = get_number(_MIN_NUMBER, _MAX_NUMBER)
+    num_b = get_number(_MIN_NUMBER, _MAX_NUMBER)
     case = '{num_a} {num_b}'.format(num_a=num_a, num_b=num_b)
 
     return (case, num_a, num_b)
@@ -32,9 +32,9 @@ def gcd_game():  # noqa: WPS210 too many local variables
     """
     (case, num_a, num_b) = _get_case()
 
-    print(question_string + case)
+    print(QUESTION_STRING + case)
     correct_answer = _get_correct_answer(num_a, num_b)
-    user_answer = prompt.string(answer_prompt)
+    user_answer = prompt.string(ANSWER_PROMPT)
 
     res = check_user_answer(user_answer, correct_answer)
 
--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -1,24 +1,24 @@
 """The progression game. Needed to find missing number in the progression."""
 
 import prompt
-from brain_games.cli import answer_prompt, question_string
+from brain_games.cli import ANSWER_PROMPT, QUESTION_STRING
 from brain_games.game import check_user_answer, get_number
 
-progression_length = 10
-min_start = -100
-max_start = 1000
-max_step = 50
+_PROGRESSION_LENGTH = 10
+_MIN_START = -100
+_MAX_START = 1000
+_MAX_STEP = 50
 
 
 def _get_progression():
     case = ''
-    number = get_number(min_start, max_start)
-    step = get_number(1, max_step)
-    index_to_hide = get_number(0, progression_length - 1)
+    number = get_number(_MIN_START, _MAX_START)
+    step = get_number(1, _MAX_STEP)
+    index_to_hide = get_number(0, _PROGRESSION_LENGTH - 1)
     hidden_number = ''
 
     i = 0
-    while i < progression_length:
+    while i < _PROGRESSION_LENGTH:
         if i == index_to_hide:
             hidden_number += str(number)
             case = '{case} {add}'.format(case=case, add='..')
@@ -41,8 +41,8 @@ def progression_game():
     """
     (case, correct_answer) = _get_progression()
 
-    print(question_string + case)
-    user_answer = prompt.string(answer_prompt)
+    print(QUESTION_STRING + case)
+    user_answer = prompt.string(ANSWER_PROMPT)
 
     res = check_user_answer(user_answer, correct_answer)
 

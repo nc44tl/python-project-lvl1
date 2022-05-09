fe154726d4f459bb06755869bf2a82a6fc4e5732
Alex Semenov <alncaatl@gmail.com>
2022-05-09 03:26:16 +0300

    Add: brain-progression game (full)

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -1,46 +1,47 @@
 """The progression game. Needed to find missing number in the progression."""
 
-from random import randint
-
 import prompt
 from brain_games.cli import answer_prompt, question_string
 from brain_games.game import check_user_answer, get_number
 
-
-def _get_operator():
-    operators = ('+', '-', '*')
-    ind = randint(0, 2)
-    return operators[ind]
-
-
-min_number = -10
-max_number = 10
+progression_length = 10
+min_start = -100
+max_start = 1000
+max_step = 50
 
 
-def _get_case():
-    a = get_number(min_number, max_number)  # noqa: WPS111 too short name
-    b = get_number(min_number, max_number)  # noqa: WPS111 too short name
-    op = _get_operator()
+def _get_progression():
+    case = ''
+    number = get_number(min_start, max_start)
+    step = get_number(1, max_step)
+    index_to_hide = get_number(0, progression_length - 1)
+    hidden_number = ''
 
-    return '{a} {op} {b}'.format(a=a, op=op, b=b)
+    i = 0
+    while i < progression_length:
+        if i == index_to_hide:
+            hidden_number += str(number)
+            case = '{case} {add}'.format(case=case, add='..')
+        else:
+            case = '{case} {add}'.format(case=case, add=str(number))
 
+        number += step
+        i += 1
 
-def _get_correct_answer(case):
-    return str(eval(case))  # noqa: S307 Use of possibly insecure function
+    return (case, hidden_number)
 
 
 def progression_game():
     """One round of brain-progression game.
 
     Returns:
-        res = result of the round (True or False),
-        user_unswer,
+        res: result of the round (boolean),
         correct_answer
+        user_unswer,
     """
-    case = _get_case()
+    (case, correct_answer) = _get_progression()
 
     print(question_string + case)
-    correct_answer = _get_correct_answer(case)
     user_answer = prompt.string(answer_prompt)
 
     res = check_user_answer(user_answer, correct_answer)

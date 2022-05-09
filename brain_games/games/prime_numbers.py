35276b72c880975d154ed5780bb7ec4c318db3ca
Alex Semenov <alncaatl@gmail.com>
2022-05-09 14:55:12 +0300

    Add: brain-prime game (full)

--- brain_games/games/prime_numbers.py
+++ brain_games/games/prime_numbers.py
@@ -5,12 +5,17 @@ from brain_games.cli import answer_prompt, question_string
 from brain_games.game import check_user_answer, get_number
 
 
-def _is_even(number):
-    return number % 2 == 0
+def _is_prime(number):
+    if number % 2 == 0:
+        return number == 2
+    d = 3
+    while d * d <= number and number % d != 0:
+        d += 2
+    return d * d > number
 
 
 def _get_correct_answer(number):
-    if _is_even(number):
+    if _is_prime(number):
         correct_answer = 'yes'
     else:
         correct_answer = 'no'
@@ -18,15 +23,15 @@ def _get_correct_answer(number):
     return (correct_answer)
 
 
-min_number = -1000000000
-max_number = 1000000000
+min_number = 1
+max_number = 500
 
 
 def prime_numbers_game():
     """One round of brain-even game.
 
     Returns:
-        res = result of the round (boolean),
+        res: result of the round (boolean),
         user_unswer,
         correct_answer
     """

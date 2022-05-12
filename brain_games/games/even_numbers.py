d729376a9895695c1c46159ad91d0a78fc91741c
Alex Semenov <alncaatl@gmail.com>
2022-05-12 13:58:24 +0300

    Fix: delete _is_even() and _get_correct_answer() in games.even_numbers

--- brain_games/games/even_numbers.py
+++ brain_games/games/even_numbers.py
@@ -8,19 +8,6 @@ MIN_NUMBER = -1000000000
 MAX_NUMBER = 1000000000
 
 
-def _is_even(number):
-    return number % 2 == 0
-
-
-def _get_correct_answer(number):
-    if _is_even(number):
-        correct_answer = 'yes'
-    else:
-        correct_answer = 'no'
-
-    return (correct_answer)
-
-
 def get_game_round():
     """Get data for the one round of the brain-even game.
 
@@ -28,7 +15,7 @@ def get_game_round():
         case: generated number,
         correct_answer
     """
-    case = randint(MIN_NUMBER, MAX_NUMBER)
-    correct_answer = _get_correct_answer(case)
+    number = randint(MIN_NUMBER, MAX_NUMBER)
+    correct_answer = 'yes' if number % 2 == 0 else 'no'
 
-    return (case, correct_answer)
+    return (number, correct_answer)

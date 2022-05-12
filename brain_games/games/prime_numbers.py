925864c731aa19bd24aa6552f6e2907dc1345a8c
Alex Semenov <alncaatl@gmail.com>
2022-05-12 15:09:52 +0300

    Fix: rewrite games.prime_numbers._is_prime, remove games.prime_numbers._get_correct_answer

--- brain_games/games/prime_numbers.py
+++ brain_games/games/prime_numbers.py
@@ -1,5 +1,6 @@
 """The prime_numbers_game. Is generated number prime or not."""
 
+from math import sqrt
 from random import randint
 
 DESCRIPTION = 'Answer "yes" if given number is prime. Otherwise answer "no".'
@@ -11,29 +12,21 @@ MAX_NUMBER = 500
 def _is_prime(number):
     if number % 2 == 0:
         return number == 2
-    d = 3
-    while d * d <= number and number % d != 0:
-        d += 2
-    return d * d > number
+    for i in range(3, int(sqrt(number)) + 1):
+        if number % i == 0:
+            return False
 
-
-def _get_correct_answer(number):
-    if _is_prime(number):
-        correct_answer = 'yes'
-    else:
-        correct_answer = 'no'
-
-    return (correct_answer)
+    return True
 
 
 def get_game_round():
     """Get data for the one round of the brain-prime game.
 
     Returns:
-        case: generated number,
+        number: generated number,
         correct_answer
     """
-    case = randint(MIN_NUMBER, MAX_NUMBER)
-    correct_answer = _get_correct_answer(case)
+    number = randint(MIN_NUMBER, MAX_NUMBER)
+    correct_answer = 'yes' if _is_prime(number) else 'no'
 
-    return (case, correct_answer)
+    return (number, correct_answer)

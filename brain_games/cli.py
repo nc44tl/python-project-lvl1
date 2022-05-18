6eea579e134105eae8f8c625072195fab62f3178
Alex Semenov <alncaatl@gmail.com>
2022-05-18 12:17:29 +0300

    Delete cli.py and /scripts/brain_games.py

--- brain_games/cli.py
+++
@@ -1,9 +0,0 @@
-"""CLI interactions."""
-
-import prompt
-
-
-def welcome_user():
-    """User acquaintance by name."""
-    name = prompt.string('May I have your name? ')
-    print(f'Hello, {name}!')
